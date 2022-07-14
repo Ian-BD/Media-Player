70ff8cb5a9abac89ad3f69725d1ca4a71a95a064
RustyTheDalek <enigma994@live.co.uk>
2022-07-14 16:20:54 +0100

    Added playlist service

---
+++ src/app/services/playlist.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlaylistService {
+
+  playlistNames: string[] = [];
+  currentPlaylist: string[] = [];
+
+  constructor() { 
+
+    //Ensure array is still created even if no playlists exist
+    this.playlistNames = JSON.parse(localStorage.getItem("playlists")) ?? [];
+
+    console.log('Playlist Component INIT');
+    console.log(this.playlistNames);
+
+  }
+
+  toggleSongAdd(filename: string){
+
+    /*We use the existence of the filename in the player 
+      to determine whether we add it or not*/
+    if(this.currentPlaylist.indexOf(filename) > -1){
+      this.currentPlaylist.splice(this.currentPlaylist.indexOf(filename), 1);
+    } else {
+      this.currentPlaylist.push(filename);
+    }
+
+    console.log(this.currentPlaylist);
+  }
+
+  savePlaylist(name){
+
+    localStorage.setItem(name, JSON.stringify(this.currentPlaylist));
+    this.playlistNames.push(name);
+    localStorage.setItem("playlists", JSON.stringify(this.playlistNames));
+  }
+}
